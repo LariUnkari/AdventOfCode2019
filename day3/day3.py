2365f65b3c8cf6c27a109948a5203635b19ad25b
lariunkari@gmail.com <lariunkari@gmail.com>
2019-12-04 17:34:37 +0200

    Day 3 part 2

--- day3/day3.py
+++ day3/day3.py
@@ -26,7 +26,7 @@ class LineInstruction:
         if LOG_LEVEL >= 2:
             print("[{0}]LineInstruction(dir={1}, len={2})".format(line_index, direction, length))
 
-    def draw_on_map(self, map_data, coordinate, do_register_crossing):
+    def walk(self, map_data, coordinate, action, *args):
         """Runs through line instructions and marks each passed coordinate down.
         Alternatively checks if a crossing was found and marks that up
         """
@@ -35,44 +35,27 @@ class LineInstruction:
                 coordinate.y += 1
                 if LOG_LEVEL >= 2:
                     print("Moved up {0}/{1} to {2}".format(i, self.length, coordinate.to_string()))
-                set_line_at_coordinate(map_data, coordinate, do_register_crossing)
+                action(map_data, coordinate, *args)
         elif self.direction == "R":
             for i in range(self.length):
                 coordinate.x += 1
                 if LOG_LEVEL >= 2:
                     print("Moved right {0}/{1} to {2}".format(i, self.length, coordinate.to_string()))
-                set_line_at_coordinate(map_data, coordinate, do_register_crossing)
-        if self.direction == "D":
+                action(map_data, coordinate, *args)
+        elif self.direction == "D":
             for i in range(self.length):
                 coordinate.y -= 1
                 if LOG_LEVEL >= 2:
                     print("Moved down {0}/{1} to {2}".format(i, self.length, coordinate.to_string()))
-                set_line_at_coordinate(map_data, coordinate, do_register_crossing)
+                action(map_data, coordinate, *args)
         elif self.direction == "L":
             for i in range(self.length):
                 coordinate.x -= 1
                 if LOG_LEVEL >= 2:
                     print("Moved left {0}/{1} to {2}".format(i, self.length, coordinate.to_string()))
-                set_line_at_coordinate(map_data, coordinate, do_register_crossing)
+                action(map_data, coordinate, *args)
 
         return coordinate
-
-def set_line_at_coordinate(map_data, coordinate, do_register_crossing):
-    """Marks down that a line passes over a coordinate.
-    Optionally adds a crossing instead if found to exist already
-    """
-
-    cs = coordinate.to_string()
-    if do_register_crossing:
-        if cs in map_data:
-            if LOG_LEVEL >= 1:
-                print("Coordinate {0} exists in map!".format(cs))
-            if map_data[cs]:
-                LINE_CROSSINGS.append(coordinate.copy())
-                if LOG_LEVEL >= 1:
-                    print("Lines cross at {0}".format(cs))
-    else:
-        map_data[coordinate.to_string()] = True
 
 
 #Initialize and read input
@@ -102,18 +85,33 @@ for index in range(len(INPUT_STRINGS)):
 #Part 1 of Day 3
 
 
-def draw_lines(map_data, line_a, line_b):
+def set_line_at_coordinate(map_data, coordinate, *args):
+    """Marks down that a line passes over a coordinate."""
+    map_data[coordinate.to_string()] = True
+
+def check_crossing_at_coordinate(map_data, coordinate, *args):
+    """Checks if a line already passed over a given coordinate."""
+    cs = coordinate.to_string()
+    if cs in map_data:
+        if LOG_LEVEL >= 1:
+            print("Coordinate {0} exists in map!".format(cs))
+        if map_data[cs]:
+            LINE_CROSSINGS.append(coordinate.copy())
+            if LOG_LEVEL >= 1:
+                print("Lines cross at {0}".format(cs))
+
+def walk_lines(map_data, line_a, line_b):
     """Draw lines on map and marks crossings"""
 
     #Draw the line on map, then register crossing on second line
     #No need to register starting point crossings
     coordinate = Coordinate(0,0)
     for instruction in line_a:
-        coordinate = instruction.draw_on_map(map_data, coordinate, False)
+        coordinate = instruction.walk(map_data, coordinate, set_line_at_coordinate)
 
     coordinate = Coordinate(0,0)
     for instruction in line_b:
-        coordinate = instruction.draw_on_map(map_data, coordinate, True)
+        coordinate = instruction.walk(map_data, coordinate, check_crossing_at_coordinate)
 
 def find_nearest_crossing_manhattan(line_crossings):
     """Seeks the closest line crossing in manhattan distance to origo"""
@@ -129,7 +127,7 @@ def find_nearest_crossing_manhattan(line_crossings):
         if distance <= 0:
             continue
 
-      #Check if closer
+        #Check if closer
         if shortest_manhattan_distance < 0 or distance < shortest_manhattan_distance:
             shortest_manhattan_distance = distance
             nearest_crossing_coordinate = crossing
@@ -145,8 +143,8 @@ def find_nearest_crossing_manhattan(line_crossings):
     return (None, -1)
 
 
-#Draw first
-draw_lines(LINE_MAP, LINES[0], LINES[1])
+#Walk the lines
+walk_lines(LINE_MAP, LINES[0], LINES[1])
 
 #Find the nearest in manhattan distance
 NEAREST_CROSSING = find_nearest_crossing_manhattan(LINE_CROSSINGS)
@@ -158,6 +156,68 @@ if NEAREST_CROSSING[1] > 0:
 #Part 2 of Day 3
 
 
-#Trace along the lines to all crossings and tally the distance along the line
-#Sum up the line distances to each crossing and find out which one has the
-#Smallest distance sum, that is the answer
+class Crossing:
+    """Defines a crossing as a coordinate and distance to it"""
+
+    def __init__(self, coordinate):
+        self.coordinate = coordinate
+        self.distance = 0
+        self.is_distance_calculated = False
+
+def increment_distance_to_crossings(map_data, coordinate, *args):
+    """Increments distance to all crossings provided in args.
+    args contains a list of crossing objects.
+    """
+    for v in args:
+        for crossing in v:
+            if crossing.is_distance_calculated:
+                continue
+            crossing.distance += 1
+            if crossing.coordinate.x == coordinate.x and crossing.coordinate.y == coordinate.y:
+                crossing.is_distance_calculated = True
+
+def calculate_crossing_distances(map_data, crossing_coordinates, line):
+    crossings = []
+    for c in crossing_coordinates:
+        crossings.append(Crossing(c.copy()))
+    coordinate = Coordinate(0,0)
+    for instruction in line:
+        coordinate = instruction.walk(map_data, coordinate, increment_distance_to_crossings, crossings)
+
+    return crossings
+
+def find_lowest_signal_delay(map_data, line_crossings, lines):
+    """Walks through the lines and calculates distance to each crossing for each line.
+    Returns the a tuple of the crossing with shortest signal delay as (int, int)
+    where first value is the index and second is the distance.
+    """
+    total_signal_delays = []
+    line_a_crossings = calculate_crossing_distances(map_data, line_crossings, lines[0])
+    line_b_crossings = calculate_crossing_distances(map_data, line_crossings, lines[1])
+
+    for index in range(len(line_a_crossings)):
+        total_signal_delays.append(line_a_crossings[index].distance)
+        print("Line A crossing at {0} with distance {1}".format(
+            line_a_crossings[index].coordinate.to_string(), line_a_crossings[index].distance))
+    for index in range(len(line_b_crossings)):
+        total_signal_delays[index] += line_b_crossings[index].distance
+        print("Line B crossing at {0} with distance {1}".format(
+            line_b_crossings[index].coordinate.to_string(), line_b_crossings[index].distance))
+
+    index_of_shortest_delay = -1
+    shortest_delay = -1
+    for index in range(len(total_signal_delays)):
+        signal_delay = total_signal_delays[index]
+        if LOG_LEVEL >= 1:
+            print("Signal delay to crossing is {0}".format(signal_delay))
+        if index_of_shortest_delay < 0 or signal_delay < shortest_delay:
+            index_of_shortest_delay = index
+            shortest_delay = signal_delay
+
+    return (index_of_shortest_delay, shortest_delay)
+
+
+#Find the crossing with lowest signal delay
+NEAREST_CROSSING = find_lowest_signal_delay(LINE_MAP, LINE_CROSSINGS, LINES)
+print("Crossing with lowest signal delay is at {0} with total combined signal delay {1}".format(
+    LINE_CROSSINGS[NEAREST_CROSSING[0]].to_string(), NEAREST_CROSSING[1]))
